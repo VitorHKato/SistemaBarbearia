ddbbe7b3b2895fc5c2d26bfa430ad0413579bfee
Vitor.Kato <vitor48kato@gmail.com>
2021-07-08 17:11:41 -0300

    Configurado banco de dados postgressql

--- SistemaBarbeariaDjango/settings.py
+++ SistemaBarbeariaDjango/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'SistemaBarbeariaDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Sistema Barbearia - Faculdade',
+        'USER': 'postgres',
+        'PASSWORD': 'qqqqqq',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'application_name': 'Sistema Barbearia - Faculdade'
+        }
     }
 }
 
