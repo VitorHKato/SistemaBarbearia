a3d8a5b200d53f3df9157f6a1a9433a3e25ad3c0
Vitor.Kato <vitor48kato@gmail.com>
2021-07-14 17:29:05 -0300

    Criado model Horario, add retorno na tela de index

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Horario(models.Model):
+    horario = models.CharField(max_length=50, null=True)
+    status = models.BooleanField(default=True)
