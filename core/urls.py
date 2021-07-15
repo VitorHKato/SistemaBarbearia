13ee9143b36ac3f268d53040952896bb53d15664
Vitor.Kato <vitor48kato@gmail.com>
2021-07-15 17:05:14 -0300

    Criado view que cria um produto

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from core.views import Home, GerenciarFuncionarios, GerenciarServicos, GerenciarProdutos
+from core.views import Home, GerenciarFuncionarios, GerenciarServicos, GerenciarProdutos, CriarProduto
 
 urlpatterns = [
     path('', Home.as_view(), name='index'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('gerenciar_servicos', GerenciarServicos.as_view(), name='gerenciar_servicos'),
     path('gerenciar_funcionarios', GerenciarFuncionarios.as_view(), name='gerenciar_funcionarios'),
     path('gerenciar_produtos', GerenciarProdutos.as_view(), name='gerenciar_produtos'),
+    path('criar_produto', CriarProduto.as_view(), name='criar_produto'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -37,8 +38,35 @@ class GerenciarServicos(View):
 
 class GerenciarProdutos(View):
     def get(self, *args, **kwargs):
-        context = {
 
+        lista_produtos = list(core.models.Produto.objects.filter(status=True))
+
+        context = {
+            'lista_produtos': lista_produtos,
         }
 
         return render(request=self.request, template_name='gerenciar_produtos.html', context=context)
+
+class CriarProduto(View):
+    def post(self, *args, **kwargs):
+        nome = self.request.POST.get('nome')
+        quantidade = self.request.POST.get('quantidade')
+        grupo = self.request.POST.get('grupo')
+        subgrupo = self.request.POST.get('subgrupo')
+
+        try:
+            core.models.Produto.objects.create(
+                nome=nome,
+                quantidade=quantidade,
+                grupo=grupo,
+                subgrupo=subgrupo,
+            ).save()
+
+            msg = 'Produto criado com sucesso.'
+        except:
+            msg = 'Erro ao criar produto.'
+        context = {
+          'msg': msg,
+        }
+
+        return JsonResponse(context, safe=False)
