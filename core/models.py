55ac3e67455b2633d12aef248f52b537c446382b
Vitor.Kato <vitor48kato@gmail.com>
2021-07-15 16:18:10 -0300

    Ajuste nos campos das models, criado requirements.txt

--- core/models.py
+++ core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import DO_NOTHING
 
-
 class Horario(models.Model):
     horario = models.CharField(max_length=50, null=True)
     status = models.BooleanField(default=True)
@@ -23,8 +22,8 @@ class Servicos(models.Model):
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, null=True)
     sobrenome = models.CharField(max_length=100, null=True)
-    dt_nascimento = models.DateTimeField(null=True)
-    situacao = models.CharField(max_length=100, null=True)  #Se está de férias, afastado, etc
+    dt_nascimento = models.DateField(null=True)
+    situacao = models.CharField(max_length=100, null=True)      #Se está de férias, afastado, etc
     salario_fixo = models.FloatField(null=True)
     status = models.BooleanField(default=True)
     cargo = models.CharField(max_length=100, null=True)
@@ -38,8 +37,8 @@ class SalarioMensal(models.Model):
 class TarefasAgendadas(models.Model):
     funcionario = models.ForeignKey(Funcionario, on_delete=DO_NOTHING)
     servico = models.ForeignKey(Servicos, on_delete=DO_NOTHING)
-    #horario_inicio = models.ForeignKey(Horario, on_delete=DO_NOTHING, related_name='Hora de inicio')
-    #horario_fim = models.ForeignKey(Horario, on_delete=DO_NOTHING, related_name='Hora de fim')
-    data = models.DateTimeField(null=True)
+    horario_inicio = models.CharField(max_length=50, null=True)
+    horario_fim = models.CharField(max_length=50, null=True)
+    data = models.DateField(null=True)
     realizado = models.BooleanField(default=True)
     status = models.BooleanField(default=True)
