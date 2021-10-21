2cb5470827dcec1381aed2faaafb10113bb0ef11
Vitor Kato <vitor48kato@gmail.com>
2021-10-21 11:47:47 -0300

    debug false

--- SistemaBarbeariaDjango/settings.py
+++ SistemaBarbeariaDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3@%bhc3hulb*0f&3y#_m&d7u56_z)n(x1s+sxsq^(-45$8n_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
