349eadb94fb2be3e06c257bb85a92600872182e4
Vitor Kato <vitor48kato@gmail.com>
2021-10-21 11:59:55 -0300

    databases

--- SistemaBarbeariaDjango/settings.py
+++ SistemaBarbeariaDjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,20 +75,23 @@ WSGI_APPLICATION = 'SistemaBarbeariaDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Sistema Barbearia - Faculdade',
-        'USER': 'postgres',
-        'PASSWORD': 'qqqqqq',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'OPTIONS': {
-            'application_name': 'Sistema Barbearia - Faculdade'
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'Sistema Barbearia - Faculdade',
+#         'USER': 'postgres',
+#         'PASSWORD': 'qqqqqq',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'application_name': 'Sistema Barbearia - Faculdade'
+#         }
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
